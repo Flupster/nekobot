b612b27f8136fbde1980fb216a5a28ca55219abb
Flupster <esptidus@gmail.com>
2023-09-17 17:37:57 +0000

    ignore untrusted subsplease releases

--- src/subsplease.ts
+++ src/subsplease.ts
@@ -1,17 +1,25 @@
 import Nyaa from "./nyaa"
 import bot from "./irc"
 
+// Nyaa gives it XiB, convert to XB (MiB=>MB, GiB=>GB, etc)
+const convertSize = (size: string) => {
+  const ssplit = size.split(" ")
+  if (ssplit.length !== 2) {
+    console.warn(new Date(), "Failed to parse size")
+    return size.replace(" ", "").replace("i", "")
+  }
+
+  return (parseFloat(ssplit[0]) * 1.048576).toFixed(2) + ssplit[1].replace("i", "")
+}
+
 Nyaa.on("release", (release) => {
   if (!release.title.includes("[SubsPlease]")) return
 
-  const ssplit = release.size.split(" ")
-  if (ssplit.length !== 2) {
-    console.warn(new Date(), "Failed to parse size")
+  if (!release.trusted) {
+    console.warn(new Date(), "Untrusted SubsPlease release", release)
     return
   }
 
-  const size = (parseFloat(ssplit[0]) * 1.048576).toFixed(2) + ssplit[1].replace("i", "")
-  const message = `[Release] ${release.title} (${size}) - ${release.guid} - ${release.torrent}`
-
+  const message = `[Release] ${release.title} (${convertSize(release.size)}) - ${release.guid} - ${release.torrent}`
   bot.say("#subsplease", message)
 })
