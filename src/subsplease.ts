e92c4616685d4b605e6c6d2bb143273c2478d121
Flupster <esptidus@gmail.com>
2023-09-17 15:25:26 +0000

    fix XiB to XB size conversion

--- src/subsplease.ts
+++ src/subsplease.ts
@@ -4,7 +4,13 @@ import bot from "./irc"
 Nyaa.on("release", (release) => {
   if (!release.title.includes("[SubsPlease]")) return
 
-  const size = release.size.replace(" ", "").replace("i", "")
+  const ssplit = release.size.split(" ")
+  if (ssplit.length !== 2) {
+    console.warn(new Date(), "Failed to parse size")
+    return
+  }
+
+  const size = (parseFloat(ssplit[0]) * 1.048576).toFixed(2) + ssplit[1].replace("i", "")
   const message = `[Release] ${release.title} (${size}) - ${release.guid} - ${release.torrent}`
 
   bot.say("#subsplease", message)
