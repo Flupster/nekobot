49c9697d091e24293315982445a750efcde5bede
Flupster <esptidus@gmail.com>
2023-09-26 17:21:39 +0000

    prevent crash flooding

--- src/nyaa.ts
+++ src/nyaa.ts
@@ -20,7 +20,6 @@ class Nyaa extends EventEmitter {
     setInterval(this.update.bind(this), 30000)
 
     this.on("release-list", (r) => this.getRelease(r.id))
-    this.update()
   }
 
   async update() {
